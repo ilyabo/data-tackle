bc2935884969bc3882648f0719ab3f4514cf7f68
Ilya Boyandin <ilya@interactivethings.com>
2013-06-14 18:30:15 +0200

    first import

---
+++ README.md
@@ -0,0 +1,12 @@
+# data-tackle
+
+A Clojure library designed for preparing data for visualizations.
+
+## Usage
+
+
+## License
+
+Copyright © 2013 Ilya Boyandin
+
+Distributed under the Eclipse Public License, the same as Clojure.
---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject data-tackle "0.1.0-SNAPSHOT"
+  :description "A Clojure library designed for preparing data for visualizations."
+  :url "http://github.com/ilyabo/data-tackle"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/data.json "0.2.2"]
+                 [org.clojure/data.csv "0.1.2"]])
